def22e072073292b5e5872d52274f811c2e110f1
Tomasz Rusin <tomek@rusin.info>
2019-03-06 16:31:31 +0100

    Add Country browser

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+var url = 'https://restcountries.eu/rest/v2/name/'
+var countriesList = document.getElementById('countries');
+
+document.getElementById('search').addEventListener('click', searchCountries);
+
+document.getElementById('country-name').addEventListener('keypress',function(e){
+    if (e.keyCode === 13) {
+        searchCountries()
+    }
+});
+
+function searchCountries() {
+    var countryName = document.getElementById('country-name').value;
+    if(!countryName.length) countryName = 'Poland';
+    fetch(url + countryName)
+        .then(function(resp) {
+            return resp.json();
+        })
+        .then(showCountriesList);
+}
+
+function showCountriesList(resp) {
+    countriesList.innerHTML = '';
+    resp.forEach(function(item){
+        var liEl = document.createElement('li');
+        liEl.innerText = item.name;
+        countriesList.appendChild(liEl);
+    });
+}
